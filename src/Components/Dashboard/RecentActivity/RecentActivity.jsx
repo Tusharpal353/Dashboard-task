324944ad071d6878dfebf261a1ea8dfcaf6c22d6
Tushar Pal <Tusharpal353@gmail.com>
2024-09-30 14:28:35 +0530

    final touch readme

--- src/Components/Dashboard/RecentActivity/RecentActivity.jsx
+++ src/Components/Dashboard/RecentActivity/RecentActivity.jsx
@@ -1,33 +1,3 @@
-// import React from "react";
-
-// const RecentActivity = () => {
-//   return (
-//     <div className="bg-[#1B204A] py-[10px] pr-[10px] pl-[24px] w-[427px] h-[258px] text-white mt-16 rounded-lg">
-//       <div>
-//         <h1 className="font-medium text-lg h-[48px]">Recent Activity</h1>
-//       </div>
-//       <div className="pt-2">
-//         <p className="font-roboto text-white font-medium text-[10px] opacity-80 h-[12px] py-3">
-//           10:40 AM, Fri 10 Sept 2021
-//         </p>
-//         <h2 className="font-medium text-lg h-[28px] py-2">You posted a new job</h2>
-//         <p className="opacity-80 font-roboto font-normal text-sm h-[40px] py-4">
-//           Kindly check the requirements and terms of work and make sure
-//           everything is right.
-//         </p>
-//         {/* Two-column layout for the activity count and button */}
-//         <div className="grid grid-cols-3 items-center gap-2 mt-4 h-[38px] pr-4">
-//           <p className="font-medium text-sm col-span-2">Today you made 12 activities</p>
-//           <button className="bg-[#FF5151] text-white py-1 px-4 rounded   whitespace-nowrap">
-//             See All Activity
-//           </button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default RecentActivity;
 
 
 import React from "react";
--- src/Components/Dashboard/Upcoming Schedule/UpcomingEventCard.jsx
+++ src/Components/Dashboard/Upcoming Schedule/UpcomingEventCard.jsx
@@ -1,29 +1,3 @@
-// import React from "react";
-// import pin_logo from "../../../Utils/Images/Cards/bi_pin-angle-fill.svg";
-// import three_dot from "../../../Utils/Images/Cards/carbon_overflow-menu-horizontal.svg";
-
-// const UpcomingCard = () => {
-//   return (
-//     <div className="flex justify-between items-center p-[12px] pr-[16px] pb-[12px] pl-[16px] bg-[#FAFAFA] border-[#E0E0E0] rounded-md"> 
-//       {/* Main container with flex, padding, and background color */}
-      
-//       {/* Left section with text */}
-//       <div className="flex flex-col mr-[95px]"> {/* Tailwind for margin-right */}
-//         <h1 className="text-lg font-semibold ">Interview with candidates</h1>
-//         <p className="text-sm text-gray-500">5 Minutes ago</p>
-//       </div>
-
-//       {/* Right section with icons */}
-//       <div className="flex items-center space-x-2"> {/* Flex to align icons horizontally */}
-       
-//         <img src={three_dot} alt="menu icon" className="w-5 h-5" />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default UpcomingCard;
-
 
 import React from "react";
 import three_dot from "../../../Utils/Images/Cards/carbon_overflow-menu-horizontal.svg";
